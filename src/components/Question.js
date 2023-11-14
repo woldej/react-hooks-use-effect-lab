6c8757d16c8c028a4342f3f2906fbae69c413e8a
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-11-14 09:50:35 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(()=>{
+
+    const timer = (timeRemaining>0)? setTimeout(()=>
+     {(setTimeRemaining(timeRemaining-1))} , 1000) :
+     onAnswered(false);
+ 
+      return function() {
+          clearTimeout(timer)
+      }
+    
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
